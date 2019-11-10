73b9a54e1ff6883b64fa4da81ae9cf66a5954cff
John <john.19col@gmail.com>
2019-11-09 19:27:08 -0500

    Add Manchester United report of won matches

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import fs from 'fs';
 
-const content: string[][] = fs
+const matches: string[][] = fs
   .readFileSync('./football.csv', { encoding: 'utf-8' })
   .split('\n')
   .map((row: string) => row.split(','));
 
-console.log(content);
+let manUnitedWins: number = 0;
+
+for (let match of matches) {
+  const wonAsLocal: boolean = match[1] === 'Man United' && match[5] === 'H';
+  const wonAsAway: boolean = match[2] === 'Man United' && match[5] === 'A';
+  if (wonAsLocal || wonAsAway) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Manchester United wins in the seasons: ${manUnitedWins}`);
