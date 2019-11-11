e5a956a37d1c16078d889722c42a008c380c4310
John <john.19col@gmail.com>
2019-11-11 12:25:22 -0500

    Update application to use new classes

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,16 @@
 import { CsvReader } from './csv/CsvReader';
-import { MatchResult, MatchRowsReader, MatchRow } from './match';
+import { MatchRowsReader, MatchRow } from './match';
+import { Report } from './report';
+import { WinsAnalyzer } from './analyzer';
+import { ConsolePrinter } from './printer';
 
 const reader: CsvReader = new CsvReader('./football.csv');
 const matchesReader: MatchRowsReader = new MatchRowsReader(reader);
 const matches: MatchRow[] = matchesReader.read();
 
-let manUnitedWins: number = 0;
+const manUnitedWins: Report = new Report(
+  new WinsAnalyzer('Man United'),
+  new ConsolePrinter()
+);
 
-for (let match of matches) {
-  const wonAsLocal: boolean =
-    match[1] === 'Man United' && match[5] === MatchResult.HomeWin;
-  const wonAsAway: boolean =
-    match[2] === 'Man United' && match[5] === MatchResult.AwayWin;
-  if (wonAsLocal || wonAsAway) {
-    manUnitedWins++;
-  }
-}
-
-console.log(`Manchester United wins in the seasons: ${manUnitedWins}`);
+manUnitedWins.run(matches);
