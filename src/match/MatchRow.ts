82aac0051cb769beed304f87e334e61b0ad31d17
John <john.19col@gmail.com>
2019-11-11 13:59:11 -0500

    Remove MatchRowField default indexes

--- src/match/MatchRow.ts
+++ src/match/MatchRow.ts
@@ -6,11 +6,11 @@ export type Referee = string;
 export type MatchRow = [Date, Team, Team, Score, Score, MatchResult, Referee];
 
 export enum MatchRowField {
-  Date = 0,
-  HomeTeam = 1,
-  AwayTeam = 2,
-  HomeScore = 3,
-  AwayScore = 4,
-  Result = 5,
-  Referee = 6
+  Date,
+  HomeTeam,
+  AwayTeam,
+  HomeScore,
+  AwayScore,
+  Result,
+  Referee
 }
